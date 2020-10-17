1fe26f162e9a66fea94040aaf959b2218d981a2c
JorgeMartins15 <jorge_tcneletronica@hotmail.com>
2020-10-17 15:36:29 -0300

    Class Components

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
+import model.entities.Components;
 import model.entities.Machine;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Machine machine = new Machine("GS300", "Empacotadora");
+		Components components = new Components(1, 5, "CLP", "000.001.0002", "Delta", "Rockwell", machine );
 		
-		System.out.println(machine);
+		System.out.println(components);
 
 	}
 
